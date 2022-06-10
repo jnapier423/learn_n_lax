2525e96d44a53207c964179edd46d944233eefed
jnapier423 <jnapier423@gmail.com>
2022-06-09 23:22:17 -0700

    Add files via upload
    template based file

--- learn_n_lax.py
+++ learn_n_lax.py
@@ -4,18 +4,15 @@ import PySimpleGUI as sg
 
 udemy = "https://www.udemy.com/"
 linkedin = "https://www.linkedin.com/learning/"
-reddit = "https://reddit.com"
-youtube = "https://youtube.com"
-roll20 = "https://roll20.net"
-twitch = "https://twitch.tv"
+
 
 #GUI Code
 
 sg.theme('Dark Grey 6')  # window theme
 
 layout = [
-    [sg.Text("Would you like to learn or relax?")],
-    [sg.Button("Learn"), sg.Button("Relax")]
+    [sg.Text("Which option would you like?")],
+    [sg.Button("option1"), sg.Button("option2")]
 ]
 window = sg.Window("Auto Web", layout, margins=(150,75))
 
@@ -26,9 +23,9 @@ while True:
     #End program if user closes window or chooses a button
     if event == sg.WIN_CLOSED:
         break
-    elif event == "Learn":
+    elif event == "option1":
         webbrowser.open_new(udemy) and webbrowser.open_new_tab(linkedin) and sys.exit()
-    elif event == "Relax":
-        webbrowser.open_new(reddit) and webbrowser.open_new_tab(youtube) and webbrowser.open_new_tab(roll20) and webbrowser.open_new_tab(twitch) and sys.exit()
+    elif event == "option2":
+        webbrowser.open_new(udemy) and webbrowser.open_new_tab(linkedin) and sys.exit()
 
 window.close()
